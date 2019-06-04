20666a7704ee8e0adba718aeffdbd5f6f7b9817d
marlonprudente <marlonmateuspr@gmail.com>
2019-06-04 01:08:32 -0300

    Rfc5424 Protocol

--- src/main/java/com/syslogclient/logclient/Main.java
+++ src/main/java/com/syslogclient/logclient/Main.java
@@ -1,20 +1,27 @@
 package com.syslogclient.logclient;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
 	private static final Logger LOG = LogManager.getLogger(Main.class);
-
+        
 	public static void main(String[] args) {
-		
+		List<String> a = new ArrayList<>();
 		LOG.debug("Printed On Debug");
 		LOG.info("Printed On Info");
 		LOG.warn("Printed On Warn");
 		LOG.error("Printed On Error");
 		LOG.fatal("Printed On Fatal");
-
 		LOG.info("Appending string: {}.", "Hi!");
+                
+                try{
+                    System.out.println(a.get(0));
+                }catch(Exception e){
+                    LOG.error(e);
+                }
 	}
 }
